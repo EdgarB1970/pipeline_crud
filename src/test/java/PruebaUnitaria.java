28f0f651234f03fbd5013fc9a60722a350d7294e
Edgar Jimmy Benavides Lopez <ejbenavides@bce.fin.ec>
2025-09-05 10:08:39 -0500

    feat: adding a unit test to fix pipeline error

---
+++ src/test/java/PruebaUnitaria.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PruebaUnitaria {
+    @Test
+    public void debeSumarDosNumeros(){
+        assertEquals(2, 1 + 1);
+    }
+}
